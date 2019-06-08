0e3838c40460641e7e8026dcbb1c6fff60056cbe
Alon Emanuel <alon@alonemanuel.com>
2019-06-08 17:43:31 +0300

    OMG this can actually recognizer speech

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from src.garcon import Garcon
 from src.recorder import Recorder
+from src.speech_recognizer import SpeechRecognizer
 
 gc = Garcon()
 
 def main():
 	gc.enter_func()
-	recorder = Recorder()
-	recorder.record()
+	# recorder = Recorder()
+	# recorder.record()
+	recognizer = SpeechRecognizer()
+	recognizer.get_text_from_mic()
 
 if __name__=='__main__':
 	main()
