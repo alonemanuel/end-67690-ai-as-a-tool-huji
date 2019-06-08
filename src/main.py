3e0002c8759ef8b0e21285dd16c1e2941970ba71
Alon Emanuel <alon@alonemanuel.com>
2019-06-08 17:12:00 +0300

    Encapsulated recorder

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from src.garcon import Garcon
+from src.recorder import Recorder
 
 gc = Garcon()
 
 def main():
 	gc.enter_func()
-	pass
+
+	recorder = Recorder()
+	recorder.
 
 if __name__=='__main__':
 	main()
--- src/recorder.py
+++ src/recorder.py
@@ -17,40 +17,40 @@ gc = Garcon()
 
 class Recorder:
 	def __init__(self, recordings_dir=RECORDING_DIR):
-		self.recordings_dir = recordings_dir
-		self.chunk = CHUNK
-		self.format = FORMAT
-		self.channels = CHANNELS
-		self.rate = RATE
-		self.record_seconds = RECORD_SECONDS
-
-		self.p = pyaudio.PyAudio()
-		self.record_idx = 0
+		self.__recordings_dir = recordings_dir
+		self.__chunk = CHUNK
+		self.__format = FORMAT
+		self.__channels = CHANNELS
+		self.__rate = RATE
+		self.__record_seconds = RECORD_SECONDS
+		self.__p = pyaudio.PyAudio()
+		self.__record_idx = 0
 
 	def record(self, record_name=''):
 		gc.enter_func()
 		record_name = record_name if record_name else f'record_n{self.record_idx}'
 		wf_name = os.path.join(RECORDING_DIR, record_name, WAVE_FN_EXT)
-		frames = self._get_record_frames()
-		self._write_recording(frames, wf_name)
+		frames = self.__get_record_frames()
+		self.__write_recording(frames, wf_name)
 
-	def _write_recording(self, frames, wf_name):
+	def __write_recording(self, frames, wf_name):
 		with wave.open(wf_name) as wf:
-			wf.setnchannels(self.channels)
-			wf.setsampwidth(self.p.get_sample_size(self.format))
-			wf.setframerate(self.rate)
+			wf.setnchannels(self.__channels)
+			wf.setsampwidth(self.__p.get_sample_size(self.__format))
+			wf.setframerate(self.__rate)
 			wf.writeframes(b''.join(frames))
 
-	def _get_record_frames(self):
-		stream = self.p.open(format=self.format, channels=self.channels,
-							 rate=self.rate, input=True,
-							 frames_per_buffer=self.chunk)
+	def __get_record_frames(self):
+		stream = self.__p.open(format=self.__format, channels=self.__channels,
+							   rate=self.__rate, input=True,
+							   frames_per_buffer=self.__chunk)
 		frames = []
-		for i in range(0, int(self.rate / self.chunk * self.record_seconds)):
-			data = stream.read(self.chunk)
+		for i in range(0,
+					   int(self.__rate / self.__chunk * self.__record_seconds)):
+			data = stream.read(self.__chunk)
 			frames.append(data)
 		gc.log('Done recording')
 		stream.stop_stream()
 		stream.close()
-		self.p.terminate()  # When should I actually terminate?
+		self.__p.terminate()  # When should I actually terminate?
 		return frames
