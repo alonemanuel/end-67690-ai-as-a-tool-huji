abb24f61002f4f30c551704fb76c4f522c9d5289
Alon Emanuel <alon@alonemanuel.com>
2019-06-08 23:51:18 +0300

    Imported librosa. Continuing tomorrow

--- src/preprocessor.py
+++ src/preprocessor.py
@@ -1,3 +1,5 @@
+import librosa as lr
+
 class Preprocessor:
 	def __init__(self, raw_x, raw_y):
 		self.raw_x, self.raw_y = raw_x, raw_y
@@ -5,3 +7,6 @@ class Preprocessor:
 
 	def preprocess(self):
 		pass
+
+	def preprocess_audio(self, wfs):
+
