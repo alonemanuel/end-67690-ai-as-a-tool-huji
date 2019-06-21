972fae1f0f1cdb5b512408ccf969de58e361b11b
Alon Emanuel <alon@alonemanuel.com>
2019-06-21 19:41:49 +0300

    Modularity rocks

--- src/frontend/debug_page.py
+++ src/frontend/debug_page.py
@@ -7,10 +7,23 @@ class DebugPage(tk.Frame):
 	def __init__(self, parent, controller):
 		tk.Frame.__init__(self, parent)
 		self.controller = controller
+		self._init_title()
+		self._init_widgets()
+
+	def _init_widgets(self):
+		self._init_navigation_buttons()
+		self._init_debugging_buttons()
+
+	def _init_debugging_buttons(self):
+		pass
+
+	def _init_title(self):
 		label = tk.Label(self, text="This is Debug mode",
-						 font=controller.title_font)
+						 font=self.controller.title_font)
 		label.pack(side="top", fill="x", pady=10)
+
+	def _init_navigation_buttons(self):
 		button = tk.Button(self, text="Go to the Menu page",
-						   command=lambda: controller._show_frame(
-							   const.MENU_PAGE))
+						   command=lambda: self.controller._show_frame(
+								   const.MENU_PAGE))
 		button.pack(side='bottom', pady=10)
